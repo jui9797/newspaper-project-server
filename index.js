4c8e25095ccc84bdf01749d508f37fb43033c2d2
Lenovo <jannatjui414@gmail.com>
2025-01-15 00:51:01 +0600

    make some api

--- index.js
+++ index.js
@@ -30,12 +30,32 @@ async function run() {
     await client.connect();
 
 const userCollection =client.db("newsdb").collection("users")
+const articlesCollection =client.db('newsdb').collection("articles")
 
 // user related api
 app.post('/users', async(req, res)=>{
     const user =req.body
+    // insert email if user doesnot exist
+    const query ={email:user.email}
+    const existingUser =await userCollection.findOne(query)
+    if(existingUser){
+        return res.send({message: 'user already exist', insertedId:null})
+    }
     const result =await userCollection.insertOne(user)
     res.send(result)
+})
+
+// get all users
+app.get('/users', async(req,res)=>{
+    const result =await userCollection.find().toArray()
+    res.send(result)
+})
+
+
+// articles related api
+app.get('/articles', async(req, res)=>{
+    const result =await articlesCollection.find().toArray()
+    res.send(result);
 })
 
 
